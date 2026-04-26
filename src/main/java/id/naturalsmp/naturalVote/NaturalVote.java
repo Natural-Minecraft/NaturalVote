285c7cef109947aaa5d4863f89b76a5a84e688e6
AnakTentara <anaktentara25@gmail.com>
2026-04-26 18:12:13 +0700

    fix: Paper plugin command registration using getCommandMap().register()

--- src/main/java/id/naturalsmp/naturalVote/NaturalVote.java
+++ src/main/java/id/naturalsmp/naturalVote/NaturalVote.java
@@ -1,7 +1,13 @@
 package id.naturalsmp.naturalVote;
 
 import id.naturalsmp.naturalVote.commands.VoteCommand;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public final class NaturalVote extends JavaPlugin {
 
@@ -11,12 +17,19 @@ public final class NaturalVote extends JavaPlugin {
         saveDefaultConfig();
 
         VoteCommand voteCommand = new VoteCommand(this);
-        if (getCommand("vote") != null) {
-            getCommand("vote").setExecutor(voteCommand);
-            getCommand("vote").setTabCompleter(voteCommand);
-        } else {
-            getLogger().warning("Format 'vote' tidak ditemukan di paper-plugin.yml!");
-        }
+        getServer().getCommandMap().register("naturalvote", new Command("vote") {
+            @Override
+            public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
+                return voteCommand.onCommand(sender, this, commandLabel, args);
+            }
+
+            @NotNull
+            @Override
+            public List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) {
+                List<String> completions = voteCommand.onTabComplete(sender, this, alias, args);
+                return completions == null ? new ArrayList<>() : completions;
+            }
+        });
 
         getServer().getPluginManager().registerEvents(new id.naturalsmp.naturalVote.gui.GUIListener(this), this);
     }
